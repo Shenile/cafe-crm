86b9250c7cc38ea95a5abb90fcf4a29d540f99e4
Shenile <sshenile07@gmail.com>
2025-03-01 01:45:45 +0530

    intial commit

---
+++ mysql-scripts/cafe_crm_ddl_script.sql
@@ -0,0 +1,175 @@
+
+CREATE DATABASE cafe_crm;
+use cafe_crm;
+
+-- ----------------- CUSTOMER ------------------
+
+CREATE TABLE customers (
+    customer_id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(256) NOT NULL,
+    email VARCHAR(256) UNIQUE,
+    mobile_no VARCHAR(15) UNIQUE,
+    date_of_birth DATE
+);
+
+
+-- --------------------------------------------
+
+-- ----------------- MENU ---------------------
+
+CREATE TABLE menu_categories (
+    category_id INT PRIMARY KEY AUTO_INCREMENT,
+    category_name VARCHAR(256) UNIQUE
+);
+
+
+CREATE TABLE menu_items (
+    item_id INT PRIMARY KEY AUTO_INCREMENT,
+    item_name VARCHAR(256) NOT NULL,
+    category_id INT NOT NULL,
+    item_price DECIMAL(10,2) NOT NULL,
+    FOREIGN KEY (category_id) REFERENCES menu_categories(category_id) ON DELETE CASCADE
+);
+
+-- -------------------------------------------
+
+-- ---------------- DISCOUNTS -----------------
+
+CREATE TABLE discount_types(
+    type_id INT PRIMARY KEY AUTO_INCREMENT,
+    type_name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE discounts (
+    discount_id INT PRIMARY KEY AUTO_INCREMENT,
+    discount_name VARCHAR(256) NOT NULL,
+    type_id INT NOT NULL,
+    discount_value DECIMAL(10,2) NOT NULL,
+    min_order_value DECIMAL(10,2) DEFAULT 0,
+    is_active BOOLEAN DEFAULT TRUE,
+    FOREIGN KEY (type_id) REFERENCES discount_types(type_id) ON DELETE CASCADE
+);
+-- --------------------------------------------
+
+-- ---------------- ORDERS --------------------
+
+CREATE TABLE orders (
+    order_id INT PRIMARY KEY AUTO_INCREMENT,
+    customer_id INT NOT NULL,
+    order_status ENUM('placed', 'completed', 'cancelled') DEFAULT 'placed',
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    FOREIGN KEY (customer_id) REFERENCES customers(customer_id) ON DELETE CASCADE
+);
+
+
+CREATE TABLE order_items (
+    order_id INT,
+    item_id INT,
+    quantity INT NOT NULL DEFAULT 1,
+    PRIMARY KEY (order_id, item_id),
+    FOREIGN KEY (order_id) REFERENCES orders(order_id),
+    FOREIGN KEY (item_id) REFERENCES menu_items(item_id)
+);
+
+
+CREATE TABLE order_payments (
+    payment_id INT PRIMARY KEY AUTO_INCREMENT,
+    order_id INT NOT NULL,
+    payment_type ENUM('cash', 'card', 'upi', 'paypal') DEFAULT 'cash',
+    payment_status ENUM('pending', 'partially_paid', 'paid', 'failed') DEFAULT 'pending',
+    amount_paid DECIMAL(10,2) NOT NULL DEFAULT 0.0,
+    FOREIGN KEY (order_id) REFERENCES orders(order_id) ON DELETE CASCADE
+);
+
+
+CREATE TABLE order_bill (
+    order_id INT PRIMARY KEY,
+    total_price DECIMAL(10,2) NOT NULL DEFAULT 0.0,
+    discount_applied DECIMAL(10,2) NOT NULL DEFAULT 0.0,
+    final_price DECIMAL(10,2) NOT NULL DEFAULT 0.0,
+    FOREIGN KEY (order_id) REFERENCES orders(order_id) ON DELETE CASCADE
+);
+
+CREATE TABLE order_discounts (
+    order_id INT NOT NULL,
+    discount_id INT NOT NULL,
+    loyalty_points_used INT NULL CHECK (loyalty_points_used >= 0),
+    discount_amount DECIMAL(10,2) NOT NULL,
+    PRIMARY KEY (order_id, discount_id),
+    FOREIGN KEY (order_id) REFERENCES orders(order_id) ON DELETE CASCADE,
+    FOREIGN KEY (discount_id) REFERENCES discounts(discount_id) ON DELETE CASCADE
+);
+
+-- --------------------------------------------
+
+-- ----------- LOYALTY PROGRAMS --------------
+
+CREATE TABLE loyalty_tiers(
+    tier_id INT PRIMARY KEY AUTO_INCREMENT,
+    tier_name VARCHAR(30) NOT NULL,
+    min_points INT NOT NULL,
+    max_points INT NOT NULL
+);
+
+CREATE TABLE loyalty_program(
+    loyalty_id INT PRIMARY KEY AUTO_INCREMENT,
+    customer_id INT NOT NULL,
+    total_points INT DEFAULT 0 CHECK (total_points >= 0),
+    tier_id INT NOT NULL,
+    FOREIGN KEY (customer_id) REFERENCES customers(customer_id) ON DELETE CASCADE,
+    FOREIGN KEY (tier_id) REFERENCES loyalty_tiers(tier_id) ON DELETE CASCADE
+);
+
+CREATE TABLE loyalty_points_log(
+    log_id INT PRIMARY KEY AUTO_INCREMENT,
+    customer_id INT NOT NULL,
+    order_id INT NULL,
+    points_earned INT NULL,
+    points_redeemed INT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    FOREIGN KEY (customer_id) REFERENCES customers(customer_id) ON DELETE CASCADE,
+    FOREIGN KEY (order_id) REFERENCES orders(order_id) ON DELETE SET NULL
+);
+
+-- --------------------------------------------
+
+-- -------- FEEDBACKS AND COMPLAINTS -----------
+
+CREATE TABLE review_categories (
+    category_id INT PRIMARY KEY AUTO_INCREMENT,
+    category_name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE customer_feedbacks (
+    feedback_id INT PRIMARY KEY AUTO_INCREMENT,
+    customer_id INT NULL,
+    order_id INT NULL,
+    item_id INT NULL,
+    category_id INT NOT NULL,
+    rating TINYINT NOT NULL CHECK (rating BETWEEN 1 AND 10),
+    comments TEXT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (customer_id) REFERENCES customers(customer_id) ON DELETE SET NULL,
+    FOREIGN KEY (order_id) REFERENCES orders(order_id) ON DELETE SET NULL,
+    FOREIGN KEY (category_id) REFERENCES review_categories(category_id) ON DELETE CASCADE,
+    FOREIGN KEY (item_id) REFERENCES menu_items(item_id) ON DELETE SET NULL
+);
+
+
+CREATE TABLE customer_complaints (
+    complaint_id INT PRIMARY KEY AUTO_INCREMENT,
+    customer_id INT NULL,
+    order_id INT NULL,
+    category_id INT NOT NULL,
+    item_id INT NULL,
+    comments TEXT NOT NULL,
+    status ENUM('pending', 'resolved', 'in_progress', 'dismissed') DEFAULT 'pending',
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (customer_id) REFERENCES customers(customer_id) ON DELETE SET NULL,
+    FOREIGN KEY (order_id) REFERENCES orders(order_id) ON DELETE SET NULL,
+    FOREIGN KEY (category_id) REFERENCES review_categories(category_id) ON DELETE CASCADE
+);
+
+-- --------------------------------------------
---
+++ mysql-scripts/cafe_crm_pre_filling_data_script.sql
@@ -0,0 +1,66 @@
+use cafe_crm;
+
+-- PRE-FILLING THE TABLES
+-- Categories
+INSERT INTO menu_categories (category_name) VALUES
+('Hot Beverages'),
+('Cold Beverages'),
+('Bakery Items'),
+('South Indian Snacks'),
+('Chaats'),
+('Desserts');
+
+-- items and price
+INSERT INTO menu_items (item_name, category_id, item_price) VALUES
+-- 🌡️ Hot Beverages
+('Coffee', 1, 20.00),
+('Tea', 1, 15.00),
+('Filter Coffee', 1, 25.00),
+('Masala Tea', 1, 20.00),
+('Sukku Malli Coffee', 1, 30.00),
+
+-- ❄️ Cold Beverages
+('Jigarthanda', 2, 60.00),
+('Nannari Sarbath', 2, 40.00),
+('Rose Milk', 2, 50.00),
+
+-- 🥐 Bakery Items
+('Egg Puffs', 3, 30.00),
+('Vegetable Puffs', 3, 25.00),
+('Banana Cake', 3, 35.00),
+
+-- 🍘 South Indian Snacks (Light Snacks Only)
+('Murukku', 4, 20.00),
+('Mixture', 4, 25.00),
+('Thattai', 4, 20.00),
+('Ribbon Pakoda', 4, 25.00),
+
+-- 🌮 Chaats
+('Masala Puri', 5, 50.00),
+('Pani Puri', 5, 40.00),
+('Samosa Chaat', 5, 45.00),
+
+-- 🍨 Desserts
+('Rasmalai', 6, 80.00),
+('Gulab Jamun', 6, 60.00),
+('Elaneer Payasam', 6, 90.00);
+
+INSERT INTO discount_types(type_name) VALUES
+('flat'), ('percentage');
+
+INSERT INTO discounts (discount_name, type_id, discount_value, min_order_value, is_active) 
+VALUES
+('10 % offer on First Order', 2, 10, 0, true),
+('20% offer on first hundreds', 2, 20, 99, true);
+
+INSERT INTO loyalty_tiers(tier_name, min_points, max_points) VALUES
+('bronze', 0, 250),
+('silver', 251, 750),
+('gold', 751, 1500),
+('platinum', 1501, 3000);
+
+INSERT INTO review_categories(category_name) VALUES
+('food & drinks'), ('service'), ('ambience'), ('pricing'), ('discount & offers');
+
+
+
